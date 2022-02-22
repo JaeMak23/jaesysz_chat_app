c37bdd85b7e9220da40ef05c8dbe0b3055f9c1f6
JaeMak23 <macklin.jaison@gmail.com>
2022-02-22 13:09:12 +0400

    Retouch of main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:jaesysz_chat_app/screens/screens.dart';
+import 'package:jaesysz_chat_app/theme.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      home: HomeScreen() ,
+    return  MaterialApp(
+      title: 'Jaesysz Chat App',
+      theme: AppTheme.light(),
+      darkTheme: AppTheme.dark(),
+      themeMode: ThemeMode.dark,
+      home:const HomeScreen() ,
     );
   }
 }
