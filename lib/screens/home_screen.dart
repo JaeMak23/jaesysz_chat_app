cf69558620aec95e11f83a8ea29140f16f3cc6a5
JaeMak23 <macklin.jaison@gmail.com>
2022-02-22 16:08:25 +0400

    Added pages to HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:jaesysz_chat_app/widgets/bottom_navigationbar.dart'as bnb;
-
+import 'package:jaesysz_chat_app/pages/pages.dart';
+import 'package:jaesysz_chat_app/widgets/bottom_navigationbar.dart' as bnb;
 
 class HomeScreen extends StatelessWidget {
-   HomeScreen({ Key? key }) : super(key: key);
+  HomeScreen({Key? key}) : super(key: key);
 
   final ValueNotifier<int> pageIndex = ValueNotifier(0);
+  final pages = const [
+    MessagesPage(),
+    NotificationsPage(),
+    CallsPage(),
+    ContactsPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        body:const Center(child: Text('HomeScreen')),
-        bottomNavigationBar:bnb.BottomNavigationBar(onItemSelected: (index){
-          pageIndex.value=index;
-        }) ,
-      );
+    return Scaffold(
+      body: ValueListenableBuilder(
+        valueListenable: pageIndex,
+        builder: (BuildContext context, int value, _) {
+          return pages[value];
+        },
+      ),
+      bottomNavigationBar: bnb.BottomNavigationBar(onItemSelected: (index) {
+        pageIndex.value = index;
+      }),
+    );
   }
 }
