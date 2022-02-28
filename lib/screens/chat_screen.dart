f31882464e908828f708c26fc5c45648ac74b520
JaeMak23 <macklin.jaison@gmail.com>
2022-02-28 16:00:50 +0400

    Added dummy message tiles to chatscreen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,8 @@
 import 'package:jaesysz_chat_app/models/models.dart';
 import 'package:flutter/material.dart';
+import 'package:jaesysz_chat_app/theme.dart';
+import 'package:jaesysz_chat_app/widgets/widgets.dart';
+import 'package:flutter/cupertino.dart' as ios;
 
 class ChatScreen extends StatelessWidget {
   static Route route(MessageData data) => MaterialPageRoute(
@@ -19,11 +22,268 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text("ChatScreen title."),
+        iconTheme: Theme.of(context).iconTheme,
+        centerTitle: false,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leadingWidth: 54,
+        leading: Align(
+          alignment: Alignment.centerRight,
+          child: IconBackground(
+            icon: ios.CupertinoIcons.back,
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ),
+        title: _AppBarTitle(messageData: messageData),
+        actions: [
+          Padding(
+            padding: const ios.EdgeInsets.symmetric(horizontal: 8.0),
+            child: Center(
+              child: IconBorder(
+                icon: ios.CupertinoIcons.video_camera_solid,
+                onTap: () {},
+              ),
+            ),
+          ),
+          ios.Padding(
+            padding: const ios.EdgeInsets.only(right: 8.0),
+            child: Center(
+              child: IconBorder(
+                icon: ios.CupertinoIcons.phone_solid,
+                onTap: () {},
+              ),
+            ),
+          ),
+        ],
       ),
-      body:const Center(
-      child: Text('ChatScreen'),
-    ),
+      body: const _DemoMessageList(),
+    );
+  }
+}
+
+class _AppBarTitle extends StatelessWidget {
+  const _AppBarTitle({
+    Key? key,
+    required this.messageData,
+  }) : super(key: key);
+
+  final MessageData messageData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Avatar.small(
+          url: messageData.profilePicture,
+        ),
+        const ios.SizedBox(
+          width: 16,
+        ),
+        ios.Expanded(
+          child: ios.Column(
+            mainAxisAlignment: ios.MainAxisAlignment.center,
+            crossAxisAlignment: ios.CrossAxisAlignment.start,
+            children: [
+              Text(
+                messageData.senderName,
+                overflow: ios.TextOverflow.ellipsis,
+                style: const TextStyle(fontSize: 14),
+              ),
+              const SizedBox(height: 2),
+              const Text(
+                'Online Now',
+                style: TextStyle(
+                  fontSize: 10,
+                  fontWeight: ios.FontWeight.bold,
+                  color: Colors.green,
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _DemoMessageList extends StatelessWidget {
+  const _DemoMessageList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: ListView(
+        children: const [
+          _DateLabel(label: 'Yesterday'),
+          _MessageTile(
+            message: 'Hi jaison! How\'s your day going on?',
+            messageDate: '12:01 PM',
+          ),
+          _MessageOwnTile(
+            message: 'I\'m fine bro. how are you.',
+            messageDate: '12:02 PM',
+          ),
+          _MessageTile(
+            message: 'Do you want starbucks?',
+            messageDate: '12:03 PM',
+          ),
+          _MessageOwnTile(
+            message: 'Would be awesome!',
+            messageDate: '12:03 PM',
+          ),
+
+          _MessageTile(
+            message: 'Coming up!',
+            messageDate: '12:04 PM',
+          ),
+          _MessageOwnTile(
+            message: 'YAY!!!',
+            messageDate: '12:04 PM',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _DateLabel extends StatelessWidget {
+  const _DateLabel({Key? key, required this.label}) : super(key: key);
+
+  final String label;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 32.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 12),
+            child: Text(
+              label,
+              style: const TextStyle(
+                fontSize: 12,
+                fontWeight: ios.FontWeight.bold,
+                color: AppColors.textFaded,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _MessageTile extends StatelessWidget {
+  const _MessageTile({
+    Key? key,
+    required this.message,
+    required this.messageDate,
+  }) : super(key: key);
+
+  final String message;
+  final String messageDate;
+
+  static const _borderRdius = 26.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: Theme.of(context).cardColor,
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(_borderRdius),
+                    topRight: Radius.circular(_borderRdius),
+                    bottomRight: Radius.circular(_borderRdius),
+                  )),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 20),
+                child: Text(message),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                messageDate,
+                style: const TextStyle(
+                  color: AppColors.textFaded,
+                  fontSize: 10,
+                  fontWeight: ios.FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _MessageOwnTile extends StatelessWidget {
+  const _MessageOwnTile({
+    Key? key,
+    required this.message,
+    required this.messageDate,
+  }) : super(key: key);
+
+  final String message;
+  final String messageDate;
+
+  static const _borderRdius = 26.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                  color: AppColors.secondary,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(_borderRdius),
+                    bottomLeft: Radius.circular(_borderRdius),
+                    bottomRight: Radius.circular(_borderRdius),
+                  )),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 20),
+                child: Text(message),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                messageDate,
+                style: const TextStyle(
+                  color: AppColors.textFaded,
+                  fontSize: 10,
+                  fontWeight: ios.FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
